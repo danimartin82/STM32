73647e56d6c4f84935560786e7b1d807ff9e1f35
Dani Martin <dani_martin_82@hotmail.com>
2024-08-31 11:49:36 +0200

    add blinking led

--- test2/App/task1/task1.c
+++ test2/App/task1/task1.c
@@ -18,7 +18,10 @@ void vTask1( void * pvParameters )
 	for(;;)
 	{
 		vPrintString(huart, (unsigned char *)"I'm task 1\n\r");
+	    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_SET);
 		vTaskDelay(1000/ portTICK_PERIOD_MS); // Wait 1000 ms
+	    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
+	    vTaskDelay(1000/ portTICK_PERIOD_MS); // Wait 1000 ms
 	}
 
 }
